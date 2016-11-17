5884f9702a7d1b65828719133319fb83b62f44f9
NikolaBorislavovHristov <nikola.b.hristov@gmail.com>
2016-11-17 16:46:07 +0200

    SYSTEM UI FLAGS constants added to the module

--- android/src/main/java/com/nh/system/ui/flags/SystemUiFlags.java
+++ android/src/main/java/com/nh/system/ui/flags/SystemUiFlags.java
@@ -1,7 +1,12 @@
 package com.nh.system.ui.flags;
 
+import android.view.View;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.common.MapBuilder;
+
+import java.util.Map;
 
 public class SystemUiFlags extends ReactContextBaseJavaModule {
     public SystemUiFlags(ReactApplicationContext reactContext) {
@@ -11,5 +16,21 @@ public class SystemUiFlags extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "SystemUiFlags";
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = MapBuilder.newHashMap();
+
+        constants.put("SYSTEM_UI_FLAG_HIDE_NAVIGATION", View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        constants.put("SYSTEM_UI_FLAG_FULLSCREEN", View.SYSTEM_UI_FLAG_FULLSCREEN);
+        constants.put("SYSTEM_UI_FLAG_IMMERSIVE", View.SYSTEM_UI_FLAG_IMMERSIVE);
+        constants.put("SYSTEM_UI_FLAG_LAYOUT_STABLE", View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        constants.put("SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION", View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        constants.put("SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN", View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        // TODO: add other SYSTEM_UI flags
+
+        return constants;
     }
 }
